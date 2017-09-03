f0df149896aa6aff54e427f86b7594ef46498f2d
Unic-1 <satyam123.sr@gmail.com>
2017-09-03 15:45:45 +0530

    New: Landing page, Map page, Notification page added with rough UI. GPS added and firebase with basic functionalities.

--- app/src/main/java/com/unic_1/hereim/LandingActivity.java
+++ app/src/main/java/com/unic_1/hereim/LandingActivity.java
@@ -1,28 +1,108 @@
 package com.unic_1.hereim;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.unic_1.hereim.Model.LocationCoordinates;
 
 public class LandingActivity extends AppCompatActivity {
 
+    LocationManager locationManager;
+    LocationListener locationListener;
+    private static Location location;
+
+    AlertDialog.Builder alertDialog;
+
     /*Gets triggered when ask button is clicked*/
     public void ask(View view) {
-
+        // Dialogue pop's up
+        alertDialog.show();
     }
 
     /*Gets triggered when answer button is clicked*/
     public void answer(View view) {
+        double lat = LandingActivity.location.getLatitude();
+        double lon = LandingActivity.location.getLongitude();
 
+        System.out.println("Latitude: "+lat+" Longitude: "+lon);
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+        // Write a message to the database
+
+        // Stores the coordinates in an object
+        LocationCoordinates locationCoordinates = new LocationCoordinates(lat, lon);
+
+        // Writes data to the firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Location");
+
+        myRef.setValue(locationCoordinates);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        createDialog();
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Listens to the location
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // Updates the location variable to the latest location
+                LandingActivity.location = location;
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            // Sets the last known location as default value
+            location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+            // Requests for any change in location
+            locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     @Override
@@ -34,15 +114,59 @@ public class LandingActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.Notification) {
+        if (item.getItemId() == R.id.Notification) {
             Intent i = new Intent(this, NotificationActivity.class);
 
             startActivity(i);
-        } else if(item.getItemId() == R.id.map) {
+        } else if (item.getItemId() == R.id.map) {
             Intent i = new Intent(this, MapsActivity.class);
 
             startActivity(i);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Checks if the permission is granted or not
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+                locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
+
+    // Creating dialog to send location request to other person
+    public void createDialog() {
+        alertDialog = new AlertDialog.Builder(LandingActivity.this);
+        alertDialog.setTitle("Request");
+        alertDialog.setMessage("Enter phone number:");
+
+        final EditText input = new EditText(LandingActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+        alertDialog.setIcon(R.drawable.ic_menu_gallery);
+
+        alertDialog.setPositiveButton("Send Request",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+                    }
+                });
+
+        alertDialog.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/unic_1/hereim/Model/LocationCoordinates.java
@@ -0,0 +1,15 @@
+package com.unic_1.hereim.Model;
+
+/**
+ * Created by unic-1 on 3/9/17.
+ */
+
+public class LocationCoordinates {
+    public double latitude;
+    public double longitude;
+
+    public LocationCoordinates(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
