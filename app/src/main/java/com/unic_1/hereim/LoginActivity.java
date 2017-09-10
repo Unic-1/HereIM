8600c8aac2679bd734c04d4908c5aacb27209d10
Bomzon <lepchajagat34@gmail.com>
2017-09-10 12:22:45 -0700

    new activity: design of login_page

---
+++ app/src/main/java/com/unic_1/hereim/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.unic_1.hereim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
